14d1826d3a7f6f6bee355d61bad67a42a9cac824
Radek Letáček <letacekr@seznam.cz>
2019-01-04 11:11:14 +0100

    Spring security in progress....

--- src/main/java/com/spring/configuration/MainConfiguration.java
+++ src/main/java/com/spring/configuration/MainConfiguration.java
@@ -1,18 +1,34 @@
 package com.spring.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.spring")
+//tady asi @PropertySource("classpath:.....persistence")
 public class MainConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -21,5 +37,16 @@ public class MainConfiguration {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    public DataSource securityDataSource(){
+
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        }catch (PropertyVetoException exc){
+            throw new RuntimeException(exc);
+        }
+        return securityDataSource;
     }
 }
