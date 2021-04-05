ee85e84f231c8af242712d648477759148ade770
Spooner6 <LetacekR@gmail.com>
2021-04-05 12:54:33 +0200

    int d

--- src/main/java/com/spring/controller/exitController.java
+++ src/main/java/com/spring/controller/exitController.java
@@ -4,30 +4,12 @@ public class exitController {
     //git test
 
     int a;
-<<<<<<< HEAD
-    int c;
-    int b;
-    int d;
     String s;
     String str;
-    String g;
-    String f;
-    String ggg;
-=======
-    String s;
-    String str;
->>>>>>> 4ee9b15733189f9ab7e34838af16a2615b741afd
 
     public exitController(int a, String s, String str) {
         this.a = a;
         this.s = s;
         this.str = str;
-<<<<<<< HEAD
-    }
-
-    public static void main(String[] args) {
-
-=======
->>>>>>> 4ee9b15733189f9ab7e34838af16a2615b741afd
     }
 }
