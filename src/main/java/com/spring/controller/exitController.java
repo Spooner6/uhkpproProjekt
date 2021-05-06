e100858170642cbe119abd6f528f2b12c036678a
Spooner6 <LetacekR@gmail.com>
2021-05-06 07:33:15 +0200

    commits

--- src/main/java/com/spring/controller/exitController.java
+++ src/main/java/com/spring/controller/exitController.java
@@ -9,10 +9,17 @@ public class exitController {
     String str2;
     String str3;
     int a2;
+    int g;
+    int c;
+    int d;
 
     public exitController(int a, String s, String str) {
         this.a = a;
         this.s = s;
         this.str = str;
+    }
+
+    public static void main(String[] args) {
+    int c = 1;
     }
 }
